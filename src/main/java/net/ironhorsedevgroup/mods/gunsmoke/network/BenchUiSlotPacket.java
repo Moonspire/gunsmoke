dd3a7ec97661154a61a3850e2fb85f96e26b895f
Moonspire <21323568+Moonspire@users.noreply.github.com>
2024-08-04 00:14:35 -0600

    Cleaned up imports

--- src/main/java/net/ironhorsedevgroup/mods/gunsmoke/network/BenchUiSlotPacket.java
+++ src/main/java/net/ironhorsedevgroup/mods/gunsmoke/network/BenchUiSlotPacket.java
@@ -6,7 +6,6 @@ import net.ironhorsedevgroup.mods.gunsmoke.gui.inventory.GunBenchMenu;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraftforge.common.capabilities.ForgeCapabilities;
